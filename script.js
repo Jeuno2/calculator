6ec528e7fd1d02a89da7e71070fdfed3ec73648e
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-08-11 13:01:59 -0500

    Update index.html script.js style.css back_arrow.svg

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
+// universal variable declarations
 let log = console.log;
+let operand_one = 0;
+let operand_two = 0;
+let operator = "";
+let result = 0;
+let para = document.querySelector('.output');
+let needsCleared = true;
+let asFirstOperator = true;
+let operatorToUse = '';
+let resultExists = false;
 
-// function add
+function resetValues() {
+    operand_one = 0;
+    operand_two = 0;
+    operator = "";
+    result = 0;
+    para.textContent = "0";
+    asFirstOperator = true;
+    dot.disabled = false;
+    needsCleared = true;
+    resultExists = false;
+}
+
+// individual arithmetic functions-------------------------------------------------------------
 function add(num1, num2) {
     return num1 + num2;
 }
 
-// function subtract
 function subtract(num1, num2) {
     return num1 - num2;
 }
 
-// function multiply
 function multiply(num1, num2) {
     return num1 * num2;
 }
 
-// function divide
 function divide(num1, num2) {
     return num1 / num2;
 }
 
-let operandOne = 0;
-let operandTwo = 0;
-let operator = "";
-
 // function operate
 function operate(oper1, oper2, operator) {
+    
+    oper1 = Number(oper1);
+    oper2 = Number(oper2);
+    resultExists = true;
+    needsCleared = true;
+
     if(operator === '+') {
         return add(oper1, oper2);
-    } //end add
+    }
     if(operator === '-') {
         return subtract(oper1, oper2);
     }
@@ -36,43 +57,103 @@ function operate(oper1, oper2, operator) {
         return multiply(oper1, oper2);
     }
     if(operator === '/') {
-        return divide(oper1, oper2);
+        if(oper2 === 0) {
+            resetValues();
+            para.textContent = 'ERROR';
+        }
+        else {
+            return divide(oper1, oper2);
+        }
     }
+
 } //end function operate
 
-let para = document.querySelector('.output');
-
-
-// const seven = document.querySelector('.btn7');
-// const eight = document.querySelector('.btn8');
-// const nine = document.querySelector('.btn9');
-// const  = document.querySelector('.btn+');
-// const seven = document.querySelector('.btn4');
-// const seven = document.querySelector('.btn5');
-// const seven = document.querySelector('.btn6');
-// const seven = document.querySelector('.btn-');
-// const seven = document.querySelector('.btn1');
-// const seven = document.querySelector('.btn2');
-// const seven = document.querySelector('.btn3');
-// const seven = document.querySelector('.btn*');
-// const seven = document.querySelector('.btnC');
-// const seven = document.querySelector('.btn0');
-// const seven = document.querySelector('.btn.');
-// const seven = document.querySelector('.btn/');
-
-const buttonArray = document.querySelectorAll('.btn');
-buttonArray.forEach((item) => {
-    item.addEventListener('click', () => {
-        para.textContent = item.textContent;
+// number buttons
+const buttonArray = document.querySelectorAll('.btn-digit');
+buttonArray.forEach((button) => {
+    button.addEventListener('click', () => {
+        //removes leading 0 so it's not a part of operand
+        if(needsCleared) {
+            para.textContent = '';
+            para.textContent = button.textContent;
+            needsCleared = false;
+        }
+        else {
+            para.textContent += button.textContent;
+        }
+        
     });
-})
+    
+}); //end section on adding event listeners to numbers/digits
 
-
-
-
-
-
-
-seven.addEventListener('click', () => {
-    para.textContent = '7';
+// equal button section
+const equalBtn = document.querySelector('#equalBtn');
+equalBtn.addEventListener('click', () => {
+    operand_two = para.textContent;
+    log(`op-one: ${operand_one} op-two: ${operand_two} operator: ${operator}`);
+    result = parseFloat(operate(operand_one, operand_two, operator).toFixed(3));
+    para.textContent = result;
+    operand_one = result;
+    operand_two = 0;
+    operator = "";
+    asFirstOperator = true;
+    log(`op-one: ${operand_one} op-two: ${operand_two} operator: ${operator}`);
 });
+
+// operator button section (+ = * /)
+const operatorArray = document.querySelectorAll('.btn-operator');
+operatorArray.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        dot.disabled = false;
+        operator = button.textContent;
+
+        if(asFirstOperator) {
+            operand_one = para.textContent;
+            para.textContent = button.textContent;
+            operatorToUse = operator;
+            asFirstOperator = false;
+        }
+        
+        else if(!asFirstOperator) {
+            operand_two = para.textContent;
+            result = parseFloat(operate(operand_one, operand_two, operatorToUse).toFixed(3));
+            para.textContent = result;
+            operand_one = result;
+            operand_two = 0;
+            operatorToUse = operator;
+        }
+
+        needsCleared = true;
+    });
+});
+
+// clear button section
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    resetValues();
+});
+
+// backSpace/backArrow button section
+let backSpace = '';
+
+const backArrow = document.querySelector('#back-arrow');
+backArrow.addEventListener('click', () => {
+    backSpace = para.textContent;
+    backSpace = backSpace.slice(0, -1);
+    para.textContent = backSpace;
+    if(para.textContent === '') {
+        resetValues();
+    }
+});
+
+// dot/period button section
+const dot = document.querySelector(`#dot`);
+dot.addEventListener('click', () => {
+    para.textContent += dot.textContent;
+    dot.disabled = true;
+});
+
+
+
+
